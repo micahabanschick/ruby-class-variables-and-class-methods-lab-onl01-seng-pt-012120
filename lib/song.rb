8dff429a147c500db8de718df295d0fcff10d1d3
Micah Banschick <micha.ban@gmail.com>
2020-01-31 16:42:39 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@artists = [] 
+  @@genres = [] 
+  
+  def initialize(name,artist,genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1 
+    if @@artists.include?(artist) == false 
+      @@artists << artist 
+  end 
+  
+  
+  
+end 
